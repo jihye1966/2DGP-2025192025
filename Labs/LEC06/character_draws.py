fd0c7449efdcdd0cf5c119fc6ab92d0e9cad2590
jihye <jihye190606@gmail.com>
2026-09-22 16:40:01 +0900

    켄버스 띄우기

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,3 +1,7 @@
+from pico2d import *
+
+open_canvas(800, 600)
+
 def move_circle():
     print('circle')
     pass
@@ -14,3 +18,5 @@ while True:
     move_circle()
     move_rectangle()
     move_triangle()
+
+close_canvas()
