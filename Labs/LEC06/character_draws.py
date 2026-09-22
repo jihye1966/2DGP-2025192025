8110bb212870f7366f9b0c95d73929d9134568bd
jihye <jihye190606@gmail.com>
2026-09-22 17:00:45 +0900

    화면에 캐릭터 그리기 기능 추가

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -5,6 +5,9 @@ character = load_image('character.png')
 
 def move_circle():
     print('circle')
+    clear_canvas()
+    character.draw(400, 300)
+    update_canvas()
     pass
 
 def move_rectangle():
